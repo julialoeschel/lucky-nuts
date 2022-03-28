1e7ce68a02cd4e1c64e7e2ea22c25e2d467ba0cb
Julia Löschel <juloeschel@gmail.com>
2022-03-28 22:34:42 +0200

    add Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,45 @@
+import styled from "styled-components";
+import Button from "../Components/Button";
+
 export default function Login() {
-  return <p>LOGIN LATER</p>;
+  return (
+    <>
+      <Form>
+        <h2>Login</h2>
+        <label htmlFor="username">
+          Username
+          <input type="text" id="username" name="name" />
+        </label>
+        <label htmlFor="passwprd">
+          Password
+          <input type="text" id="password" name="password" />
+        </label>
+        <Button>log me in!</Button>
+      </Form>
+    </>
+  );
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-template-rows: 100px 1fr 1fr 1fr;
+  padding: 100px 20px;
+  gap: 20px;
+  font-family: "Special Elite", cursive;
+
+  Button {
+    margin-top: 30px;
+  }
+
+  label {
+    align-item: end;
+    align-self: center;
+  }
+
+  input {
+    display: block;
+    width: 80%;
+    height: 25px;
+    margin: 5px 0;
+  }
+`;
